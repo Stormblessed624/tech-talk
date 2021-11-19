12a5bd64c595ddf50a1411086a6c92bd89674ffa
Matt <matt.white.624@gmail.com>
2021-11-19 03:20:39 -0700

    added models

---
+++ config/controller.js
@@ -0,0 +1,15 @@
+//import sequelize 
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to db
+let sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS talk_db;
+
+CREATE DATABASE talk_db;
