331686d176d6aefef36b619cd7624d902d1dab4a
Matt <matt.white.624@gmail.com>
2021-11-19 03:12:42 -0700

    fixed dependencies

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection')
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: { expire: 10 * 60 * 1000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+//turn on routes 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+//turn on connection to db and server 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
-})
+    app.listen(PORT, () => console.log('Now listening'));
+});
