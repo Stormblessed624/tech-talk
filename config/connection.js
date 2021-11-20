641f45da6b939d1a5a9d9b0c9b504a6e8047b842
Matt <matt.white.624@gmail.com>
2021-11-19 23:33:31 -0700

    added handlebars and styling

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // create connection to db
 let sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
